73b6ca8cc78f6f3aee1f3acc357b256be38a0d38
Benedikt Kuehn <41262108+benekuehn@users.noreply.github.com>
2023-12-28 12:19:14 +0100

    init styleX but falling back to css modules for now

--- apps/component-demo/next.config.js
+++ apps/component-demo/next.config.js
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const StylexPlugin = require("@stylexjs/webpack-plugin");
+const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new StylexPlugin({
+        // filename: 'styles.css',
+        // get webpack mode and set value for dev
+        dev: config.mode === "development",
+        // Required for CSS variable support
+        appendTo: (name) => name.endsWith(".css"),
+        filename: path.join(__dirname, "app", "stylex-bundle.css"),
+        unstable_moduleResolution: {
+          // The module system to be used.
+          // Use this value when using `ESModules`.
+          type: "commonJS",
+          // The absolute path to the root directory of your project.
+          rootDir: __dirname,
+        },
+      }),
+    );
+
+    return config;
+  },
+  transpilePackages: ['@repo/style'],
+};
+
+module.exports = nextConfig;
--- packages/style/src/button/button.tsx
+++ packages/style/src/button/button.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import * as stylex from '@stylexjs/stylex';
+import styles from './button.module.css'
 
 export const Button = () => {
     return (
-        <button>Button</button>
+        <button className={styles.button}>Button</button>
     )
 }
