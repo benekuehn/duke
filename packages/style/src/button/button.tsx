a7ee3423344665bc2d9d8801f77e7baac9d195f2
benekuehn <41262108+benekuehn@users.noreply.github.com>
2024-01-04 11:30:54 +0100

    fix exporting in /style

--- packages/style/src/button/button.tsx
+++ packages/style/src/button/button.tsx
@@ -14,7 +14,7 @@ const styles = stylex.create({
         gap: '8px',
         borderRadius: '4px',
         fontFamily: "'Inter', sans-serif",
-        fontSize: '14px',
+        fontSize: '0,875 rem',
         fontStyle: 'normal',
         fontWeight: 500,
         lineHeight: 'normal',
@@ -70,7 +70,7 @@ const variants = stylex.create({
 });
 
 export type ButtonProps = {
-    variant: keyof typeof variants;
+    variant?: keyof typeof variants;
     size?: 'icon';
     isDisabled?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
